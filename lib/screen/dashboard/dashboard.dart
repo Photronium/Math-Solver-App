72e453d30c030df97672573b23d2188b993b3483
Miley Cyrus <62071233+nkhachao@users.noreply.github.com>
2020-06-16 21:33:17 +0700

    1 change
    Added Performance Score widget to Dashboard view

--- lib/screen/dashboard/dashboard.dart
+++ lib/screen/dashboard/dashboard.dart
@@ -11,94 +11,46 @@ class Dashboard extends StatelessWidget {
         centerTitle: true,
       ),
       drawer: DrawTab(),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(30),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        crossAxisCount: 1,
         children: <Widget>[
-          Column(children: <Widget>[
-            Container(
-                height: 100.0,
-                margin: EdgeInsets.all(15.0),
+          PerformanceScore()
+        ],
+      )
+    );
+  }
+}
+
+class PerformanceScore extends StatelessWidget {
+  var score = 8;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+                height: 500.0,
                 child: FlatButton(
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(30.0),
                   ),
                   color: Colors.white,
-                  child: ListTile(
-                    leading: Image(
-                      image:
-                      AssetImage('assets/images/iconSimplexMethod-06.png'),
-                      height: 100.0,
-                    ),
-                    title: Text("Simplex Method",
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text("Performance Score",
                         style: TextStyle(fontSize: 20.0)),
-                    subtitle: Text(
-                        "The most basic method to solve Linear Program",
-                        style: TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontWeight: FontWeight.w100)),
-                    trailing: Icon(Icons.navigate_next),
-                  ),
+                    Text(
+                        score.toString(),
+                        style: TextStyle(fontSize: 80)),
+                    Text("See more about how your score is calculated →",
+                        style: TextStyle(fontSize: 14.0)),
+                  ]),
                   onPressed: () {
                     print("Simplex");
                   },
-                )),
-            Container(
-                height: 100.0,
-                margin: EdgeInsets.all(15.0),
-                child: FlatButton(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                  ),
-                  color: Colors.white,
-                  child: ListTile(
-                    leading: Image(
-                      image: AssetImage(
-                          'assets/images/iconBranchAndBound-06.png'),
-                      height: 100.0,
-                    ),
-                    title: Text("Branch and Bound",
-                        style: TextStyle(fontSize: 20.0)),
-                    subtitle: Text(
-                        'The "divide and conquer" method to solve LP with integer conditions',
-                        style: TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontWeight: FontWeight.w100)),
-                    trailing: Icon(Icons.navigate_next),
-                  ),
-                  onPressed: () {
-                    print("Branch and Bound");
-                  },
-                )),
-            Container(
-                height: 100.0,
-                margin: EdgeInsets.all(15.0),
-                child: FlatButton(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                  ),
-                  color: Colors.white,
-                  onPressed: () {
-                    print("Cutting Plane");
-                  },
-                  child: ListTile(
-                    leading: Image(
-                      image:
-                      AssetImage('assets/images/iconCuttingPlane-06.png'),
-                      height: 100.0,
-                    ),
-                    title: Text("Cutting Plane",
-                        style: TextStyle(fontSize: 20.0)),
-                    subtitle: Text(
-                        "Refine a feasible set or objective function by means of linear inequalities",
-                        style: TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontWeight: FontWeight.w100)),
-                    trailing: Icon(Icons.navigate_next),
-                  ),
-                )),
-          ]),
-        ],
-      ),
-    );
+                ));
   }
 }
