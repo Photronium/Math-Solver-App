5dc191a624fc895864978de21a41ba111c50c192
Miley Cyrus <62071233+nkhachao@users.noreply.github.com>
2020-06-17 20:24:43 +0700

    1 major change
    - Added Performance History to Dashboard view

--- lib/screen/dashboard/dashboard.dart
+++ lib/screen/dashboard/dashboard.dart
@@ -16,7 +16,11 @@ class Dashboard extends StatelessWidget {
         body: ListView(
           scrollDirection: Axis.vertical,
           padding: const EdgeInsets.all(padding),
-          children: <Widget>[PerformanceScore(), ScoreChart()],
+          children: <Widget>[
+            PerformanceScore(),
+            ScoreChart(),
+            PerformanceHistory()
+          ],
         ));
   }
 }
@@ -93,7 +97,8 @@ class _ScoreChart extends State<ScoreChart> {
               children: <Widget>[
                 Padding(
                   padding: EdgeInsets.only(bottom: 20),
-                child: Text("Score Analysis", style: TextStyle(fontSize: 20.0)),
+                  child:
+                      Text("Score Analysis", style: TextStyle(fontSize: 20.0)),
                 ),
                 SizedBox(
                   height: 350,
@@ -111,23 +116,110 @@ class _ScoreChart extends State<ScoreChart> {
                                   height: 300 - scores[i] * 30,
                                 ),
                                 Container(
-                                  color: Color.fromRGBO(50, (scores[i] * 25).toInt(),
-                                      255, i%16==0 ? 1 : 0.2),
+                                  color: Color.fromRGBO(
+                                      50,
+                                      (scores[i] * 25).toInt(),
+                                      255,
+                                      i % 16 == 0 ? 1 : 0.2),
                                   width: 4,
                                   height: scores[i] * 30,
                                 ),
                               ])),
                           Padding(
                             padding: EdgeInsets.only(top: 4),
-                          child: Text(i%16==0 ? scores[i].toString():""),
+                            child:
+                                Text(i % 16 == 0 ? scores[i].toString() : ""),
                           )
-                        ]
-                        )
+                        ])
                     ],
                   ),
                 ),
                 Text("See more about how your score is calculated →",
                     style: TextStyle(fontSize: 14.0)),
+              ]),
+          onPressed: () {
+            print("Simplex");
+          },
+        ));
+  }
+}
+
+class PerformanceHistory extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _PerformanceHistory();
+  }
+}
+
+class _PerformanceHistory extends State<PerformanceHistory> {
+  double average = 8;
+  double oldAverage = 6.5;
+  double difference = 0;
+
+  void initState() {
+    super.initState();
+    difference = average / oldAverage - 1;
+  }
+
+  Widget build(BuildContext context) {
+    return Container(
+        height: 250.0,
+        padding: EdgeInsets.only(top: padding),
+        child: FlatButton(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+          color: Colors.white,
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(bottom: 20),
+                  child: Text("Performance History",
+                      style: TextStyle(fontSize: 20.0)),
+                ),
+                  Column(
+                    children: <Widget>[
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Padding(
+                          padding: EdgeInsets.only(top: 10),
+                          child: Container(
+                            color: Color.fromRGBO(
+                                50, (average * 25).toInt(), 255, 1),
+                            width: average * 25,
+                            height: 20,
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Padding(
+                          padding: EdgeInsets.only(top: 10),
+                          child: Container(
+                            color: Color.fromRGBO(
+                                50, (oldAverage * 25).toInt(), 255, 1),
+                            width: oldAverage * 25,
+                            height: 20,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 26),
+                        child: Row(children: <Widget>[
+                          Text("You performed ",
+                              style: TextStyle(fontSize: 16.0)),
+                          Text((100 * difference).toInt().toString(),
+                              style: TextStyle(fontSize: 19.0)),
+                          Text(difference > 0 ? "% better" : "% worse",
+                              style: TextStyle(fontSize: 16.0)),
+                          Text(" than before", style: TextStyle(fontSize: 16.0))
+                        ]),
+                      ),
+                    ],
+                  ),
               ]),
           onPressed: () {
             print("Simplex");
