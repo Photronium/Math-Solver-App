f2a6d1fc7d7b1b9c1d0d3762e13240479996d596
tohoangdao <63783327+tohoangdao@users.noreply.github.com>
2020-06-18 08:34:28 +0700

    Test Page

---
+++ showresult.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class resultpage extends StatefulWidget {
+  int marks;
+  resultpage({Key key , @required this.marks}) : super(key : key);
+  @override
+  _resultpageState createState() => _resultpageState(marks);
+}
+
+class _resultpageState extends State<resultpage> {
+  int marks;
+
+  _resultpageState(this.marks);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Test',
+            style: TextStyle(fontWeight: FontWeight.w300)),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text('You scored $marks'),
+      ),
+    );
+  }
+}
