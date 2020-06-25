9b43030e0ce56436c3d7c5f78a10391346e67f66
Nguyễn Hoàng Dũng <50265963+dfighter1312@users.noreply.github.com>
2020-06-25 10:09:55 -0400

    1 minor change

--- lib/screen/problem_solving/simplex_solver.dart
+++ lib/screen/problem_solving/simplex_solver.dart
@@ -5,42 +5,85 @@ final constraintController = TextEditingController();
 final equationController = [];
 final conditionController = [];
 class SimplexSolver {
-  List<int> equation = [];
-  List<List<int>> condition = [];
+  List<int> _equation = [];
+  List<List<int>> _condition = [];
+
+  List<int> getEquation(){
+    return _equation;
+  }
+
+  List<List<int>> getCondition(){
+    return _condition;
+  }
+
+  void addEquation(int a){
+    _equation.add(a);
+  }
+
+  void addCondition(List<int> a){
+    _condition.add(a);
+  }
 
   SimplexSolver(){
     for(int i = 0; i < int.parse(variableController.text); i++){
-      equation.add(int.parse(equationController[i].text));
+      addEquation(int.parse(equationController[i].text));
     }
     for(int i = 0; i < int.parse(constraintController.text); i++){
       List<int> oneCondition = [];
       for(int j = 0; j < int.parse(variableController.text) + 1; j++){
         oneCondition.add(int.parse(conditionController[i][j].text));
       }
-      condition.add(oneCondition);
+      addCondition(oneCondition);
     }
   }
 
   String printEquation(){
     String equationString = "Y =";
-    for(int i = 0; i < equation.length; i++){
-      equationString += " ${equation[i]}X${i+1} ";
-      if(i < equation.length - 1)
+    for(int i = 0; i < getEquation().length; i++){
+      equationString += " ${getEquation()[i]}X${i+1} ";
+      if(i < getEquation().length - 1)
         equationString += "+";
     }
     return equationString;
   }
 
+  int checkNegation(List<int> a){
+    int least = 0, least_idx = -1;
+    for(int i = 0; i < a.length - 1; i++){
+      if(least > a[i]){
+        least == a[i];
+        least_idx = i;
+      }
+    }
+    return least_idx;
+  }
+
+  int choosePivot(List<List<int>> a, int least_idx){
+
+  }
+
+  void solve(){
+    //Set up the matrix
+    List<List<int>> matrix = List<List<int>>.from(getCondition());
+    List<int> equation_matrix = List<int>.from(getEquation());
+    for(int i = 0; i < equation_matrix.length; i++){
+      equation_matrix[i] *= -1;
+    }
+    equation_matrix.add(0);
+    matrix.add(equation_matrix);
+
+  }
+
   String printCondition(){
     String conditionString = "";
-    for(int i = 0; i < condition.length; i++){
+    for(int i = 0; i < getCondition().length; i++){
       int j;
-      for(j = 0; j < condition[i].length - 1; j++){
-        conditionString += " ${condition[i][j]}X${j+1} ";
-        if(j < condition[i].length - 2)
+      for(j = 0; j < getCondition()[i].length - 1; j++){
+        conditionString += " ${getCondition()[i][j]}X${j+1} ";
+        if(j < getCondition()[i].length - 2)
           conditionString += "+";
       }
-      conditionString += "<= ${condition[i][j]} \n";
+      conditionString += "<= ${getCondition()[i][j]} \n";
     }
     return conditionString;
   }
