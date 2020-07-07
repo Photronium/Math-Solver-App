ef7da714e2c5ec2e9d131309355b2d23f6db4631
ducbao811 <bao.dao811@hcmut.edu.vn>
2020-07-07 17:31:21 +0700

    Add button to navigate to feedback and solution page. Also create feedback page

--- lib/screen/learning_material/methods.dart
+++ lib/screen/learning_material/methods.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'feedback/feedback.dart';
 
 
 Future getMethod(String method) async{
@@ -26,31 +27,61 @@ class DetailedDescriptionState extends State<DetailedDescription> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: FutureBuilder(
-          future: getMethod(method),
-          builder: (_, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(
-                child: Text("Loading ..."),
-              );
-            }
-            else {
-              return Container(
-                child: Text(
-                  snapshot.data.data['definition'],
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0, fontFamily: 'Times New Roman'),
-                ),
-                margin: EdgeInsets.all(15.0),
-                padding: const EdgeInsets.all(3.0),
-                decoration: BoxDecoration(
-                  border: Border.all(),
-                  color: Colors.white,
-                ),
-              );
-            }
-          }
-      ),
+    return Column(
+      children: <Widget>[
+        Container(
+          child: FutureBuilder(
+              future: getMethod(method),
+              builder: (_, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(
+                    child: Text("Loading ..."),
+                  );
+                }
+                else {
+                  return Container(
+                    child: Text(
+                      snapshot.data.data['definition'],
+                      style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20.0, fontFamily: 'Times New Roman'),
+                    ),
+                    margin: EdgeInsets.all(15.0),
+                    padding: const EdgeInsets.all(3.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                      color: Colors.white,
+                    ),
+                  );
+                }
+              }
+          ),
+        ),
+        ButtonBar(
+          buttonMinWidth: 150.0,
+          alignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Container(
+              child: RaisedButton(
+                child: Text('General Solution'),
+                color: Colors.green,
+                onPressed: () {},
+              ),
+            ),
+            Container(
+              child: RaisedButton(
+                child: Text('Feedback'),
+                color: Colors.green,
+                onPressed: () {
+                  Navigator.pop(context);
+                  Navigator.push(context,
+                      new MaterialPageRoute(builder: (context) => FeedbackPage())
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }}
 
