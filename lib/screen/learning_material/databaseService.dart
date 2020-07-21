2006a8004e25d1d071e09306727faebbf33dfef4
ducbao811 <bao.dao811@hcmut.edu.vn>
2020-07-21 14:15:56 +0700

    Create databaseService.dart
    Create connection to fetch data from database.

---
+++ lib/screen/learning_material/databaseService.dart
@@ -0,0 +1,20 @@
+// This file include function to connect to database
+// It will hold a reference to the database
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+// This method is used to get a method from the database
+Future getMethod(String method) async {
+
+    var db = Firestore.instance;
+    QuerySnapshot qn = await db.collection("methods").getDocuments();
+    DocumentSnapshot Rmethod;
+
+    int i = 0;
+    qn.documents.forEach((document) {
+      if (document.data['name'] == method) Rmethod = document;
+    });
+
+    return Rmethod;
+
+}
